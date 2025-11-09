96970df52c9bd238147c307eb12566a00b966b21
strivers2324 <strivers2324@gmail.com>
2025-11-10 01:11:38 +0600

    Update App.tsx
    Update

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -16,6 +16,12 @@ function App() {
   const location = useLocation();
 
   useEffect(() => {
+    if (window.history.scrollRestoration) {
+      window.history.scrollRestoration = 'manual';
+    }
+  }, []);
+
+  useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.pathname]);
 
